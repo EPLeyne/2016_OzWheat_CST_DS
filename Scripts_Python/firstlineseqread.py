dce3ff5cbb3eafa3f90289ecd528fa1cf360cb61
Emmett Leyne <emmettleyne@gmail.com>
2019-04-23 15:16:32 +1000

    A working Python script that takes the first line of the fastq file and adds it to a csv file with the file name

--- Scripts_Python/firstlineseqread.py
+++ Scripts_Python/firstlineseqread.py
@@ -1,16 +1,15 @@
-# Working well with 1 file but is still not looping through more than 1 file
+# Import the names of the files in a directory
 
+files = glob.glob('data/fastq/*.fastq')
 
-import glob
-import csv
-
-files = glob.glob('../*.fastq')
-print(files)
-num = 0
-for file in files:
+for file in files:   
     with open (file, 'r') as in_file:
-        with open ('first_lines.csv', 'w') as out_file:
+        with open ('first_lines2.csv', 'a') as out_file:
+            num = 0
             for x in in_file:
-                if num<1:
+                if num<1 and x[0] == '@':
+                    out_file.write(file)
+                    out_file.write('\t')
                     out_file.write(x[0:27])
-                    num += 1
+                    out_file.write('\n')
+                    num =+ 1
