7619b37e357bd131bd748071c068e01a1ed8c0fe
Gordon McLachlan <mcl226@pearcey-login.cm.hpc>
2019-05-02 09:33:19 +1000

    Scripts for finally merged metadata

--- Scripts_Python/firstlineseqread.py
+++ Scripts_Python/firstlineseqread.py
@@ -1,17 +1,18 @@
 # Script to iterate through the .fastq files in the directory and write the first 4 items of metadata from the first line to a file. 
 import glob
+import gzip
 
-files = glob.glob('data/fastq/*.fastq')   #glob.glob to make a list of all the files in the directory (need to change path as needed)
+globfiles = glob.glob('/OSM/CBR/AF_DATASCHOOL/input/2019-04-12_Transcritome/CA73YANXX_8_161220_BPO--030_Other_CGAGGCTG-CGTCTAAT_R_161128_SHADIL_LIB2500_M002_R1.fastq.gz')   #glob.glob to make a list of all the files in the directory (need to change path as needed)
 
-for file in files:                                          # for each file in the list
-    with open (file, 'r') as in_file:                       # open that file name as 'r' (read) as in_file
-        with open ('first_lines2.csv', 'a') as out_file:    # create the .csv file as 'a' (append) as out_file
+for filenames in globfiles:
+    with gzip.open (filenames, 'rb') as f:
+        in_file = f.read()                  # open that file name as 'r' (read) as in_file
+        with open ('/OSM/CBR/AF_DATASCHOOL/output/epl/first_lines.csv', 'a') as out_file:    # create the .csv file as 'a' (append) as out_file
             num = 0                                         # create a variable 'num' with the value 0
-            for x in in_file:                               # for each item in infile
-                if num<1 and x[0] == '@':                   # if num is less than 1 and the first character in the line in @ (poss not req)
-                    out_file.write(file)                    # write the file name to the file
+            if num < 1:                                 # if num is less than 1
+                    out_file.write(filenames)               # write the file name to the file
                     out_file.write('\t')                    # then a tab
-                    out_file.write(x[0:27])                 # write the first 27 characters from the line to the file
+                    out_file.write(in_file[0:27])                 # write the first 27 characters from the line to the file
                     out_file.write('\n')                    # then a new line
                     num =+ 1                                # add 1 to the num variable (to end the loop)
 
--- Scripts_R/MergeFiles.R
+++ Scripts_R/MergeFiles.R
@@ -2,12 +2,52 @@
 library(tidyverse)
 
 #read files to be merged
-met#we need to join files to form metadata file
+#we need to join files to form metadata file
 
-adataCSV <- read(/OSM/CBR/AF_DATASCHOOL/output/gmcl/metadatCSV.csv)
-fastqfiles_split <- read(/OSM/CBR/AF_DATASCHOOL/output/gmcl/fastqfiles_split.csv)
+metadataCSV <- read.table( '/OSM/CBR/AF_DATASCHOOL/output/gmcl/metadataCSV.csv')
+fastqfiles_split <- read.csv( '/OSM/CBR/AF_DATASCHOOL/output/gmcl/fastqfiles_split.csv', sep=',')
 
 
 # merge two data frames by "Index_Sequence"
-MetadataMerged <- merge(data metadataCSV, data fastqfiles_split, by="Index_Sequence")
+MetadataMerged <- merge(metadataCSV, fastqfiles_split, by="Index_Sequence")
+
+#need to create output file tab delimited for MetadataMerged
+#need to specify which directory to send it to, currently in same directory.
+#write.table(MetadataMerged, "/OSM/CBR/AF_DATASCHOOL/output/gmcl/MetadataMerged.txt", sep="\t")
+#However rowline number keeps apperaing in output file.
+
+#so creat output file as .CSV
+write.csv(MetadataMerged, "/OSM/CBR/AF_DATASCHOOL/output/gmcl/MetadataMerged.csv")
+
+#pull in file with first line from every file, need to refine file before merging
+first_lines <- read.table( '/OSM/CBR/AF_DATASCHOOL/output/epl/first_lines.csv', sep = ("_"))
+first_lines2 <- first_lines[ -c (1,2,3,4,5,6,7,9,10,11,12,13)]
+first_lines3 <- separate(first_lines2, V14, c("rep_instrumentNAME", "run_ID", "flowcel_ID", "flowcell_Lane"), ":")
+first_lines4 <- separate(first_lines3, rep_instrumentNAME, c("rep", "instrument_Name"), "@")
+first_lines5 <- separate(first_lines4, rep, c("rep", "b"), ".fastq.gz")
+first_lines6 <- first_lines5[ -c (3)]
+# Rename a column 1 to Index_Sequence
+names(first_lines6)[1]<-"Index_Sequence"
+#so creat output file as .CSV
+write.csv(first_lines6, "/OSM/CBR/AF_DATASCHOOL/output/gmcl/first_lines_refined.csv")
+
+# merge two data frames by "Index_Sequence"
+MetadataMergedFinally1 <- merge(MetadataMerged, first_lines6, by=c("Index_Sequence","rep"))
+
+# Rename a column 1 to Index_Sequence
+names(MetadataMergedFinally1)[5]<-"Vendor.Sample.Name"
+#Need to redorder columns
+MetadataMergedFinally2 <- MetadataMergedFinally1[c(4,3,2,1,5,6,7,8,9)]
+#Need to insert 0 infront of 1-9, to make 01-09, for Sample_ID
+MetadataMergedFinally2$Sample_ID <- sprintf("%02d", as.numeric(MetadataMergedFinally2$Sample_ID))
+
+#sort Treatment & Sample ID
+MetadataMergedFinally3 <- MetadataMergedFinally2[order(MetadataMergedFinally2$Sample_ID),]
+MetadataMergedFinally4 <- MetadataMergedFinally3[order(MetadataMergedFinally3$Treatment),]
+
+
+
+#so creat output file as .CSV
+write.csv(MetadataMergedFinally3, "/OSM/CBR/AF_DATASCHOOL/output/gmcl/MetadataMergedFinallyv1.csv")
+write.csv(MetadataMergedFinally4, "/OSM/CBR/AF_DATASCHOOL/output/gmcl/MetadataMergedFinallyv2.csv")
 
