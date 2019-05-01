57b37006026aacbc08da68d1813472047308621a
Emmett Leyne <emmettleyne@gmail.com>
2019-05-01 14:20:05 +1000

    Cleaned up script

--- Scripts_Python/firstlineseqread.py
+++ Scripts_Python/firstlineseqread.py
@@ -6,16 +6,10 @@ globfiles = glob.glob('/OSM/CBR/AF_DATASCHOOL/input/2019-04-12_Transcritome/*.fa
 #the directory (need to change path as needed)
 
 for filenames in globfiles:
-    with gzip.open (filenames, 'rb') as f:
-        in_file = f.read()                  # open that file name as 'r' (read) as in_file
+    with gzip.open (filenames, 'rb') as f:      # open that file name as 'r' (read) as in_file
+        in_file = f.read()                  
         with open ('/OSM/CBR/AF_DATASCHOOL/output/epl/first_lines.csv', 'a') as out_file:    # create the .csv file as 'a' (append) as out_file
-            num = 0                                         # create a variable 'num' with the value 0
-            if num < 1:                                 # if num is less than 1
+            num = 0                             # create a variable 'num' with the value 0
+            if num < 1:                         # if num is less than 1
                     out_file.write(filenames + '\t' + str(in_file[0:27]) + '\n')               # write the file name to the file
-#                    out_file.write('\t')                    # then a tab
-#                    out_file.write(in_file[0:27])                 # write the first 27 characters from the line to the file
-#                    out_file.write('\n')                    # then a new line
-                    num =+ 1                                # add 1 to the num variable (to end the loop)
-
-
-# Need to add script down here to delimit and delete unwanted data, might be better in R but will look it up.
+                    num =+ 1                    # add 1 to the num variable (to end the loop)
