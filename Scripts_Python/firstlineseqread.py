2ba6f97d070c41985d48b959703d37e6859d00a7
Emmett Leyne <ley015@pearcey-login.cm.hpc>
2019-05-01 14:12:42 +1000

    Working Python script for all files to return csv files of the first line

--- Scripts_Python/firstlineseqread.py
+++ Scripts_Python/firstlineseqread.py
@@ -2,7 +2,8 @@
 import glob
 import gzip
 
-globfiles = glob.glob('/OSM/CBR/AF_DATASCHOOL/input/2019-04-12_Transcritome/CA73YANXX_8_161220_BPO--030_Other_CGAGGCTG-CGTCTAAT_R_161128_SHADIL_LIB2500_M002_R1.fastq.gz')   #glob.glob to make a list of all the files in the directory (need to change path as needed)
+globfiles = glob.glob('/OSM/CBR/AF_DATASCHOOL/input/2019-04-12_Transcritome/*.fastq.gz')   #glob.glob to make a list of all the files in 
+#the directory (need to change path as needed)
 
 for filenames in globfiles:
     with gzip.open (filenames, 'rb') as f:
@@ -10,10 +11,10 @@ for filenames in globfiles:
         with open ('/OSM/CBR/AF_DATASCHOOL/output/epl/first_lines.csv', 'a') as out_file:    # create the .csv file as 'a' (append) as out_file
             num = 0                                         # create a variable 'num' with the value 0
             if num < 1:                                 # if num is less than 1
-                    out_file.write(filenames)               # write the file name to the file
-                    out_file.write('\t')                    # then a tab
-                    out_file.write(in_file[0:27])                 # write the first 27 characters from the line to the file
-                    out_file.write('\n')                    # then a new line
+                    out_file.write(filenames + '\t' + str(in_file[0:27]) + '\n')               # write the file name to the file
+#                    out_file.write('\t')                    # then a tab
+#                    out_file.write(in_file[0:27])                 # write the first 27 characters from the line to the file
+#                    out_file.write('\n')                    # then a new line
                     num =+ 1                                # add 1 to the num variable (to end the loop)
 
 
